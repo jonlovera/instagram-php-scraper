6164e3b6210b57f908d7b477c7ff7b920837abbb
Jonathan Lovera <jonathan@rootplace.com>
2018-10-31 02:37:21 +1100

    Added cache

--- src/instagram-php-scraper.php
+++ src/instagram-php-scraper.php
@@ -5,18 +5,22 @@
      * Developed by Jonathan Lovera
      * MIT license
      */
+     require './simple-cache.php';
 
-    // Defining the basic cURL function
-    $user = "USERNAME_HERE";
-    function curl($url) {
-        $ch = curl_init();  // Initialising cURL
-        curl_setopt($ch, CURLOPT_URL, $url);    // Setting cURL's URL option with the $url variable passed into the function
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); // Setting cURL's option to return the webpage data
-        $data = curl_exec($ch); // Executing the cURL request and assigning the returned data to the $data variable
-        curl_close($ch);    // Closing cURL
-        return $data;   // Returning the data from the function
+    // Defining username
+    $user = "zuck";
+
+    // Defining cache folder
+    $cacheFolder = 'instagram-cache';
+    if (!file_exists($cacheFolder)) {
+      mkdir($cacheFolder, 0777, true);
     }
-    $scraped_website = curl("https://www.instagram.com/$user/");
+
+    $cache = new Gilbitron\Util\SimpleCache();
+    $cache->cache_path = $cacheFolder . '/';
+    $cache->cache_time = 3600;
+
+    $scraped_website = $cache->get_data("user-$user", "https://www.instagram.com/$user/");
     $document = new DOMDocument();
   	$document->loadHTML($scraped_website);
   	$selector = new DOMXPath($document);
